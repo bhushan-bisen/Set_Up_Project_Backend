471c956706f837f2746495f54f198ec284e09a41
Somdutta18 <somdutta.mitra@accolitedigital.com>
2022-03-24 01:01:45 +0530

    First Commit

---
+++ src/main/java/com/example/accolite/AccoliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.accolite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccoliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccoliteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/accolite/controller/recruiterController.java
@@ -0,0 +1,5 @@
+package com.example.accolite.controller;
+
+public class recruiterController {
+
+}
